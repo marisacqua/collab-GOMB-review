32a2d9f0676da8a5023eb083f57eaa55f49f3b1b
FvanSteen <floydvansteen@gmail.com>
2024-10-10 15:32:39 +0200

    cleaned up main and removed old operations

--- src/main.py
+++ src/main.py
@@ -4,16 +4,6 @@ Combining operations
 from operations import summation, subtraction
 
 
-# This is some code that doesn't work, and therefore
-# it is commented out. We should remove it at
-# some point:
-
-# def perform_operation(num1, num2, operation):
-#     if operation = "add":
-#         result = summation(num1, num2)
-
-#     return result
-
 def perform_operation(num1, num2, operation):
     if operation == "add":
         result = summation(num1, num2)
